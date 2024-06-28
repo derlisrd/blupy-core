e0d1768547951809975922fc8fb21c744525fb1b
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-06-27 20:37:34 -0400

    cambio desde dev

--- app/Traits/Helpers.php
+++ app/Traits/Helpers.php
@@ -4,7 +4,7 @@ namespace App\Traits;
 
 use App\Services\FarmaService;
 use App\Services\InfinitaService;
-use Illuminate\Support\Facades\Log;
+//use Illuminate\Support\Facades\Log;
 
 trait Helpers
 {
