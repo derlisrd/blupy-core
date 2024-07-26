fa189d804bd66feeabef32de8387cd8178251e85
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-07-26 09:14:06 -0400

    rest

--- routes/rest.php
+++ routes/rest.php
@@ -8,5 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('login',[AuthController::class,'login']);
 
 Route::middleware(Authenticate::using('api'))->group(function(){
-
+    Route::get('/solicitudes');
 });
