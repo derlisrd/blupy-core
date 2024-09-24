d48a165bf3ae9945bd08044999ee9bbada43098c
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-09-24 10:23:04 -0400

    mt nume

--- app/Http/Controllers/BlupyApp/QRController.php
+++ app/Http/Controllers/BlupyApp/QRController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Notificacion;
 use App\Services\BlupyQrService;
 use App\Services\PushExpoService;
-use App\Services\SupabaseService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -14,67 +13,72 @@ class QRController extends Controller
 {
     private $webserviceBlupyQRCore;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->webserviceBlupyQRCore = new BlupyQrService();
     }
 
-    public function autorizar(Request $req){
+    public function autorizar(Request $req)
+    {
         try {
             $user = $req->user();
             $cliente = $user->cliente;
             $parametrosPorArray = [
-                'id'=>$req->id,
-                'documento'=>$cliente->cedula,
-                'numero_cuenta'=>$req->numerocuenta,
-                'telefono'=>$req->telefono,
-                'ip'=>$req->ip(),
-                'localizacion'=>$req->localizacion,
-                'adicional'=>$req->adicional
+                'id' => $req->id,
+                'documento' => $cliente->cedula,
+                'numero_cuenta' => $req->numerocuenta,
+                'telefono' => $req->telefono,
+                'ip' => $req->ip(),
+                'localizacion' => $req->localizacion,
+                'adicional' => $req->adicional
             ];
             $blupy = $this->webserviceBlupyQRCore
                 ->autorizarQR($parametrosPorArray);
             $data = (object) $blupy['data'];
+
+            if (property_exists($data, 'results')) {
+
                 $noti = new PushExpoService();
                 $tokens = $user->notitokens();
-                $noti->send($tokens,'Compra en comercio','Se ha registrado una compra en comercio');
+                $noti->send($tokens, 'Compra en comercio', 'Se ha registrado una compra en comercio');
 
                 Notificacion::create([
-                    'user_id'=>$user->id,
-                    'title'=>'Compra en comercio',
-                    'body'=> $data->results['info']
+                    'user_id' => $user->id,
+                    'title' => 'Compra en comercio',
+                    'body' => $data->results['info']
                 ]);
-
-
+            }
             return response()->json([
-                'success'=>$data->success,
-                'message'=>$data->message
-            ],$blupy['status']);
+                'success' => $data->success,
+                'message' => $data->message
+            ], $blupy['status']);
         } catch (\Throwable $th) {
             Log::error($th);
             throw $th;
             return response()->json([
-                'success'=>false,
-                'message'=>'Error de servidor'
-            ],500);
+                'success' => false,
+                'message' => 'Error de servidor'
+            ], 500);
         }
     }
 
-    public function consultar($id){
+    public function consultar($id)
+    {
 
         $blupy = $this->webserviceBlupyQRCore->consultarQR($id);
         $data = (object) $blupy['data'];
 
-        if($data->success){
+        if ($data->success) {
             return response()->json([
-                'success'=>$data->success,
-                'message'=>'',
-                'results'=>$data->results,
-            ],$blupy['status']);
+                'success' => $data->success,
+                'message' => '',
+                'results' => $data->results,
+            ], $blupy['status']);
         }
 
         return response()->json([
-            'success'=>$data->success,
-            'message'=>$data->message
-        ],$blupy['status']);
+            'success' => $data->success,
+            'message' => $data->message
+        ], $blupy['status']);
     }
 }
