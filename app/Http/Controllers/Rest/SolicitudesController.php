d9b1138cdd1a2ec9e88ea977f6f0ce731ce6070f
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-01 09:10:26 -0400

    fecha

--- app/Http/Controllers/Rest/SolicitudesController.php
+++ app/Http/Controllers/Rest/SolicitudesController.php
@@ -212,7 +212,6 @@ class SolicitudesController extends Controller
         $ayer = Carbon::yesterday()->format('Y-m-d');
         $fechaInicioMes =  $desde;
         $hoy = $hasta;
-        $fechaHoy = $hoy;
         $primeraHora = '00:00:00';
         $ultimaHora = '23:59:59';
 
@@ -227,33 +226,33 @@ class SolicitudesController extends Controller
         $externos = Cliente::where('funcionario',0)->where('asofarma',0)->count();
         $asociaciones = Cliente::where('funcionario',0)->where('asofarma',1)->count();
 
-        $registrosDelMes = Cliente::whereBetween('created_at',[$fechaInicioMes,$fechaHoy])->count();
+        $registrosDelMes = Cliente::whereBetween('created_at',[$fechaInicioMes,$hoy . ' 23:59:59'])->count();
         $registrosSemana = Cliente::whereBetween('created_at',[$lunes,$domingo])->count();
         $registrosHoy = Cliente::whereBetween('created_at',[$hoy.' 00:00:00',$hoy . ' 23:59:59'])->count();
         $registrosAyer = Cliente::whereBetween('created_at',[$ayer.$primeraHora, $ayer . $ultimaHora])->count();
 
 
         $solicitudesRechazadas = SolicitudCredito::where('tipo',1)->where('estado_id',11)->count();
-        $rechazadosHoy = SolicitudCredito::whereBetween('created_at',[$hoy.' 00:00:00',$fechaHoy])->where('tipo',1)->where('estado_id',11)->count();
+        $rechazadosHoy = SolicitudCredito::whereBetween('created_at',[$hoy.' 00:00:00',$hoy . ' 23:59:59'])->where('tipo',1)->where('estado_id',11)->count();
         $rechazadosSemana = SolicitudCredito::whereBetween('created_at',[$lunes,$domingo])->where('tipo',1)->where('estado_id',11)->count();
-        $rechazadosMes = SolicitudCredito::whereBetween('created_at',[$fechaInicioMes,$fechaHoy])->where('tipo',1)->where('estado_id',11)->count();
+        $rechazadosMes = SolicitudCredito::whereBetween('created_at',[$fechaInicioMes,$hoy . ' 23:59:59'])->where('tipo',1)->where('estado_id',11)->count();
 
         $solicitudesTotales = SolicitudCredito::where('tipo',1)->where('estado_id','<>',null)->count();
-        $solicitudesHoy = SolicitudCredito::whereBetween('created_at',[$hoy.' 00:00:00',$fechaHoy])->where('tipo',1)->count();
+        $solicitudesHoy = SolicitudCredito::whereBetween('created_at',[$hoy.' 00:00:00',$hoy . ' 23:59:59'])->where('tipo',1)->count();
         $solicitudesAyer = SolicitudCredito::whereBetween('created_at',[$ayer.$primeraHora, $ayer . $ultimaHora])->where('tipo',1)->count();
         $solicitudesSemana = SolicitudCredito::whereBetween('created_at',[$lunes,$domingo])->where('tipo',1)->count();
-        $solicitudesMes = SolicitudCredito::where('tipo',1)->whereBetween('created_at',[$fechaInicioMes,$fechaHoy])->count();
+        $solicitudesMes = SolicitudCredito::where('tipo',1)->whereBetween('created_at',[$fechaInicioMes,$hoy . ' 23:59:59'])->count();
 
         $solicitudesPendientes = SolicitudCredito::where('tipo',1)->where('estado_id',5)->count();
-        $pendientesHoy = SolicitudCredito::whereBetween('created_at',[$hoy.' 00:00:00',$fechaHoy])->where('tipo',1)->where('estado_id',5)->count();
+        $pendientesHoy = SolicitudCredito::whereBetween('created_at',[$hoy.' 00:00:00',$hoy . ' 23:59:59'])->where('tipo',1)->where('estado_id',5)->count();
         $pendientesSemana = SolicitudCredito::whereBetween('created_at',[$lunes,$domingo])->where('tipo',1)->where('estado_id',5)->count();
-        $pendientesMes = SolicitudCredito::where('tipo',1)->whereBetween('created_at',[$fechaInicioMes,$fechaHoy])->where('estado_id',5)->count();
+        $pendientesMes = SolicitudCredito::where('tipo',1)->whereBetween('created_at',[$fechaInicioMes,$hoy . ' 23:59:59'])->where('estado_id',5)->count();
 
 
         $solicitudVigentes = SolicitudCredito::where('tipo',1)->where('estado_id',7)->count();
-        $vigentesHoy = SolicitudCredito::whereBetween('updated_at',[$hoy.' 00:00:00',$fechaHoy])->where('tipo',1)->where('estado_id',7)->count();
+        $vigentesHoy = SolicitudCredito::whereBetween('updated_at',[$hoy.' 00:00:00',$hoy . ' 23:59:59'])->where('tipo',1)->where('estado_id',7)->count();
         $vigentesSemana = SolicitudCredito::whereBetween('updated_at',[$lunes,$domingo])->where('tipo',1)->where('estado_id',7)->count();
-        $vigentesMes = SolicitudCredito::whereBetween('updated_at',[$fechaInicioMes,$fechaHoy])->where('tipo',1)->where('estado_id',7)->count();
+        $vigentesMes = SolicitudCredito::whereBetween('updated_at',[$fechaInicioMes,$hoy . ' 23:59:59'])->where('tipo',1)->where('estado_id',7)->count();
         $porcentajeDeRechazo = number_format( ($solicitudesRechazadas  * 100 / $solicitudesTotales),2 );
 
         return response()->json([
