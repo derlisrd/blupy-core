0447dd2ea10fe2c5b2611fcf12d98c687fd6e8f7
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-01 13:48:34 -0400

    vendedores model

--- app/Models/Vendedor.php
+++ app/Models/Vendedor.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Vendedor extends Model
 {
     use HasFactory;
+    protected $table = 'vendedores';
+    protected $fillable = [
+        'cedula',
+        'nombre',
+        'punto',
+        'direccion',
+        'organigrama',
+        'qr_generado',
+        'supervisor_id',
+        'encargado_id'
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
 }
