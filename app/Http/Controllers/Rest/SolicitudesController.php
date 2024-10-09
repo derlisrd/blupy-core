aaf4fdae67fea329c8ffc3276950bd1fb72263ea
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-09 15:21:07 -0300

    asociaciones

--- app/Http/Controllers/Rest/SolicitudesController.php
+++ app/Http/Controllers/Rest/SolicitudesController.php
@@ -254,6 +254,8 @@ class SolicitudesController extends Controller
         $rechazadosMes = SolicitudCredito::whereBetween('created_at',[$fechaInicioMes,$hoy . ' 23:59:59'])->where('tipo',1)->where('estado_id',11)->count();
 
         $solicitudesFuncionarios = Cliente::where('s.tipo',1)->where('clientes.funcionario',1)->join('solicitud_creditos as s','clientes.id','=','s.cliente_id')->count();
+        $solicitudesFuncionariosAprobados = Cliente::where('s.tipo',1)->where('s.estado_id',5)->where('clientes.funcionario',1)->join('solicitud_creditos as s','clientes.id','=','s.cliente_id')->count();
+        $solicitudesAsociaciones = Cliente::where('s.tipo',1)->where('clientes.asofarma',1)->join('solicitud_creditos as s','clientes.id','=','s.cliente_id')->count();
 
         $solicitudesTotales = SolicitudCredito::where('tipo',1)->where('estado_id','<>',null)->count();
         $solicitudesHoy = SolicitudCredito::whereBetween('created_at',[$hoy.' 00:00:00',$hoy . ' 23:59:59'])->where('tipo',1)->count();
@@ -309,7 +311,9 @@ class SolicitudesController extends Controller
                 'solicitudesSemana'=>$solicitudesSemana,
                 'solicitudesMes'=>$solicitudesMes,
 
-                'solicitudesFuncionarios'=>$solicitudesFuncionarios
+                'solicitudesFuncionarios'=>$solicitudesFuncionarios,
+                'solicitudesFuncionariosAprobados'=>$solicitudesFuncionariosAprobados,
+                'solicitudesAsociaciones'=>$solicitudesAsociaciones,
 
             ]
         ]);
