b3b3b73faf57c8df7424665296acf85a59c4f2e3
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-09 09:12:47 -0300

    view

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\Public\UserController as UserPublic;
 use App\Http\Controllers\BlupyApp\ValidacionesController;
 use App\Http\Controllers\Public\VersionController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
+
 
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/confirmar-nuevo-dispositivo',[DeviceController::class,'confirmarNuevoDispositivo']);
@@ -85,20 +85,4 @@ Route::middleware('auth:api')->group(function(){
 
 });
 
-Route::get('/app', function (Request $request) {
-    $userAgent = $request->header('User-Agent');
 
-    // Detectar Android
-    if (stripos($userAgent, 'Android') !== false) {
-        return redirect()->away('intent://blupy/#Intent;scheme=blupy;package=com.teo.blupy;end');
-    }
-    // Detectar iOS
-    elseif (stripos($userAgent, 'iPhone') !== false || stripos($userAgent, 'iPad') !== false) {
-        // Intentar abrir la app primero
-        return response()->view('redirect_to_ios');
-    }
-    // Para otros dispositivos, redirigir al sitio web
-    else {
-        return redirect()->away('https://blupy.com.py');
-    }
-});
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,24 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/app', function (Request $request) {
+    $userAgent = $request->header('User-Agent');
+
+    // Detectar Android
+    if (stripos($userAgent, 'Android') !== false) {
+        return redirect()->away('intent://blupy/#Intent;scheme=blupy;package=com.teo.blupy;end');
+    }
+    // Detectar iOS
+    elseif (stripos($userAgent, 'iPhone') !== false || stripos($userAgent, 'iPad') !== false) {
+        // Intentar abrir la app primero
+        return response()->view('redirect_to_ios');
+    }
+    // Para otros dispositivos, redirigir al sitio web
+    else {
+        return redirect()->away('https://blupy.com.py');
+    }
 });
