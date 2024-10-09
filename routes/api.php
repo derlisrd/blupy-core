84a45ccc6c9a2582160d9ca6d9082afaa54327ec
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-09 09:08:42 -0300

    deeplink

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\BlupyApp\UserController as UserPrivate;
 use App\Http\Controllers\Public\UserController as UserPublic;
 use App\Http\Controllers\BlupyApp\ValidacionesController;
 use App\Http\Controllers\Public\VersionController;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login',[AuthController::class,'login']);
@@ -82,4 +83,23 @@ Route::middleware('auth:api')->group(function(){
 
     Route::get('/sucursales-cercanas',[FarmaController::class,'sucursalesCercanas']);
 
+});
+
+
+Route::get('/app', function (Request $request) {
+    $userAgent = $request->header('User-Agent');
+
+    // Detectar Android
+    if (stripos($userAgent, 'Android') !== false) {
+        return redirect()->away('intent://blupy/#Intent;scheme=blupy;package=com.teo.blupy;end');
+    }
+    // Detectar iOS
+    elseif (stripos($userAgent, 'iPhone') !== false || stripos($userAgent, 'iPad') !== false) {
+        // Intentar abrir la app primero
+        return response()->view('redirect_to_ios');
+    }
+    // Para otros dispositivos, redirigir al sitio web
+    else {
+        return redirect()->away('https://blupy.com.py');
+    }
 });
