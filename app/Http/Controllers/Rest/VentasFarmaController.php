5806b9c5593341d779f385db6f642eb80b961576
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-16 17:54:14 -0300

    ventas mes jobs

--- app/Http/Controllers/Rest/VentasFarmaController.php
+++ app/Http/Controllers/Rest/VentasFarmaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Rest;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\ProcesarVentasDelDiaFarmaJobs;
 use App\Models\Cliente;
 use App\Models\Venta;
 use App\Services\FarmaService;
@@ -13,47 +14,25 @@ use Illuminate\Support\Facades\Validator;
 
 class VentasFarmaController extends Controller
 {
-    public function VentasDelDia(Request $req){
-        $validator = Validator::make($req->only('fecha'),['fecha' => 'required|date_format:Y-m-d']);
+    public function VentasDelMes(Request $req){
+        $validator = Validator::make($req->only(['fecha_inicio', 'fecha_fin']), [
+            'fecha_inicio' => 'required|date_format:Y-m-d',
+            'fecha_fin' => 'required|date_format:Y-m-d|after_or_equal:fecha_inicio'
+        ]);
 
-        if ($validator->fails())
-            return response()->json(['success'=>false,'message' => $validator->errors()->first()], 400);
-
-        $farmaService = new FarmaService();
-
-        $res = (object)$farmaService->ventasRendidas($req->fecha);
-        $data = (object) $res->data;
-        $results = [];
-        if(property_exists($data,'result')){
-            $ventas = $data->result;
-            foreach($ventas as $v){
-                $venta = Venta::where('codigo',$v['ventCodigo'])->first();
-                if(!$venta){
-                    $fechaFormateada = Carbon::parse($v['ventFecha'])->format('Y-m-d H:i:s');
-                    $cliente = Cliente::where('cedula',$v['cedula'])->first();
-                    $cliente_id = $cliente ? $cliente->id : null;
-                    $ventaCreada = Venta::create([
-                        'cliente_id'=>$cliente_id,
-                        'codigo'=>$v['ventCodigo'],
-                        'documento'=>$v['cedula'],
-                        'adicional'=>$v['clieCodigoAdicional'],
-                        'factura_numero'=>$v['ventNumero'],
-                        'importe'=>$v['ventTotBruto'],
-                        'descuento'=>$v['ventTotDescuento'],
-                        'importe_final'=>$v['ventTotNeto'],
-                        'forma_pago'=>$v['frpaAbreviatura'],
-                        'forma_codigo'=>$v['frpaCodigo'],
-                        'descripcion'=>$v['ventCodigo'],
-                        'sucursal'=>$v['estrDescripcion'],
-                        'fecha'=>$fechaFormateada,
-                        'forma_venta'=>$v['ventTipo']
-                    ]);
-                    array_push($results,$ventaCreada);
-                }
-            }
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'message' => $validator->errors()->first()], 400);
         }
 
-        return response()->json(['success'=>true,'message'=>"Ventas ingresadas", 'results'=>$results]);
+        $fechaInicio = Carbon::parse($req->fecha_inicio);
+        $fechaFin = Carbon::parse($req->fecha_fin);
 
+        // Recorrer cada día del rango
+        for ($date = $fechaInicio; $date->lte($fechaFin); $date->addDay()) {
+            ProcesarVentasDelDiaFarmaJobs::dispatch($date->format('Y-m-d'));
+        }
+
+
+        return response()->json(['success' => true, 'message' => "El Job para procesar las ventas ha sido despachado."]);
     }
 }
---
+++ app/Jobs/ProcesarVentasDelDiaFarmaJobs.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Cliente;
+use App\Models\Venta;
+use App\Services\FarmaService;
+use App\Services\SupabaseService;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcesarVentasDelDiaFarmaJobs implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $fecha;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($fecha)
+    {
+        $this->fecha = $fecha;
+    }
+
+    public function handle()
+    {
+        $farmaService = new FarmaService();
+
+        $res = (object)$farmaService->ventasRendidas($this->fecha);
+        $data = (object) $res->data;
+        $results = [];
+        if (property_exists($data, 'result')) {
+            $ventas = $data->result;
+            foreach ($ventas as $v) {
+                $venta = Venta::where('codigo', $v['ventCodigo'])->first();
+                if (!$venta) {
+                    $fechaFormateada = Carbon::parse($v['ventFecha'])->format('Y-m-d H:i:s');
+                    $cliente = Cliente::where('cedula', $v['cedula'])->first();
+                    $cliente_id = $cliente ? $cliente->id : null;
+                    $ventaCreada = Venta::create([
+                        'cliente_id' => $cliente_id,
+                        'codigo' => $v['ventCodigo'],
+                        'documento' => $v['cedula'],
+                        'adicional' => $v['clieCodigoAdicional'],
+                        'factura_numero' => $v['ventNumero'],
+                        'importe' => $v['ventTotBruto'],
+                        'descuento' => $v['ventTotDescuento'],
+                        'importe_final' => $v['ventTotNeto'],
+                        'forma_pago' => $v['frpaAbreviatura'],
+                        'forma_codigo' => $v['frpaCodigo'],
+                        'descripcion' => $v['ventCodigo'],
+                        'sucursal' => $v['estrDescripcion'],
+                        'fecha' => $fechaFormateada,
+                        'forma_venta' => $v['ventTipo']
+                    ]);
+                    array_push($results, $ventaCreada);
+                }
+            }
+        }
+
+        SupabaseService::LOG('ventas_farma','ingresadas fecha '.$this->fecha);
+    }
+
+}
