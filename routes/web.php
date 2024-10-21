35b593961875c2ec5fc8a6a31d595aa677fc9fea
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-21 16:43:49 -0300

    terminos

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/terminos', function () {
     return view('terminos');
 });
+Route::get('/terminos2', function () {
+    return view('terminos');
+});
