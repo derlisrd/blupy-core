19e2742bdcb552ba54366f29a3b3b0e50bdb832e
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-21 09:43:38 -0300

    vincular vendedor

--- app/Http/Controllers/BlupyApp/VendedorController.php
+++ app/Http/Controllers/BlupyApp/VendedorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\BlupyApp;
 use App\Http\Controllers\Controller;
 use App\Models\Vendedor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class VendedorController extends Controller
 {
@@ -23,13 +24,27 @@ class VendedorController extends Controller
     }
 
     public function vincular(Request $req){
+        $validator = Validator::make($req->all(),[
+            'id'=>'required|exists:vendedores,id'
+        ],[ 'vendedor_id.exists' => 'El vendedor seleccionado no existe en el sistema.' ]);
+
+        if($validator->fails())
+            return response()->json(['success'=>false,'message'=>$validator->errors()->first() ], 400);
+
+
         $user = $req->user();
+        if ($user->vendedor_id !== null) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Vendedor ya vinculado',
+            ], 400);
+        }
 
-
+        $user->vendedor_id = $req->id;
+        $user->save();
         return response()->json([
             'success'=>false,
-            'message'=>'Vendedor ya vinculado',
-            'vendedor'=>$user->vendedor_id
+            'message'=>'Vendedor ya vinculado'
         ],400);
     }
 }
