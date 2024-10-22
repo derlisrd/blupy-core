071b9a95c055c1c52be5ba09e43a2add6cc67cdf
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-22 16:21:29 -0300

    ya solucionamos

--- routes/console.php
+++ routes/console.php
@@ -5,8 +5,7 @@ use App\Jobs\UpdateSolicitudesJobs;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Schedule;
 
-//Schedule::job(new UpdateSolicitudesJobs());
-//Schedule::job(ProcesarVentasDelDiaFarmaJobs::dispatch(Carbon::yesterday()->format('Y-m-d')));
+Schedule::job(new UpdateSolicitudesJobs())->dailyAt('00:48');
+Schedule::job(ProcesarVentasDelDiaFarmaJobs::dispatch(Carbon::yesterday()->format('Y-m-d')))->dailyAt('00:40');
 
 //->timezone('America/Asuncion')
-//->dailyAt('03:00');
