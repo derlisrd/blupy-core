b65c2a6a146793ef6202597bfa7bda8225fc38a6
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-22 16:16:37 -0300

    para migrar

--- routes/console.php
+++ routes/console.php
@@ -5,8 +5,8 @@ use App\Jobs\UpdateSolicitudesJobs;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(new UpdateSolicitudesJobs());
-Schedule::job(ProcesarVentasDelDiaFarmaJobs::dispatch(Carbon::yesterday()->format('Y-m-d')));
+//Schedule::job(new UpdateSolicitudesJobs());
+//Schedule::job(ProcesarVentasDelDiaFarmaJobs::dispatch(Carbon::yesterday()->format('Y-m-d')));
 
 //->timezone('America/Asuncion')
 //->dailyAt('03:00');
