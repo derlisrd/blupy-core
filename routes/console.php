3a7b18c4a216006fccd67012eae86e059069e416
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-23 09:25:11 -0300

    jobs

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,8 @@ use App\Jobs\UpdateSolicitudesJobs;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Schedule;
 
-//Schedule::job(UpdateSolicitudesJobs::dispatch())->dailyAt('00:48');
-//Schedule::job(ProcesarVentasDelDiaFarmaJobs::dispatch(Carbon::yesterday()->format('Y-m-d')))->dailyAt('00:40');
+Schedule::job(UpdateSolicitudesJobs::dispatch());
+Schedule::job(ProcesarVentasDelDiaFarmaJobs::dispatch(Carbon::yesterday()->format('Y-m-d')));
+//->dailyAt('00:40');
 
 //->timezone('America/Asuncion')
