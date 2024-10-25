d275188869ffcddcd4b5c5efb8e1e41d3cf76da6
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-10-25 12:46:24 -0300

    vendedor de farma

--- app/Http/Controllers/Farma/VendedoresController.php
+++ app/Http/Controllers/Farma/VendedoresController.php
@@ -4,34 +4,93 @@ namespace App\Http\Controllers\Farma;
 
 use App\Http\Controllers\Controller;
 use App\Models\Vendedor;
+use App\Services\FarmaService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Validator;
 
 class VendedoresController extends Controller
 {
     public function generarQRVendedor(Request $r){
-        $validator = Validator::make($r->all(), [
+
+        $ip = $r->ip();
+        $executed = RateLimiter::attempt($ip,$perTwoMinutes = 10,function() {});
+        if (!$executed)
+            return response()->json(['success'=>false, 'message'=>'Demasiadas peticiones. Espere 1 minuto.' ],500);
+
+        $validator = Validator::make($r->only(['cedula']), [
             'cedula' => 'required',
         ]);
-        if ($validator->fails()) {
+        if ($validator->fails())
             return response()->json([
                 'success'=>false,
                 'message'=> $validator->errors()
             ], 400);
-        }
+
 
         $vendedor = Vendedor::where('cedula',$r->cedula)->first();
         if($vendedor){
             return response()->json([
                 'success'=>true,
-                'id'=> $vendedor->id,
+                'message'=> '',
                 'results'=>$vendedor
             ]);
         }
 
         return response()->json([
             'success'=>false,
-            'id'=> null
+            'results'=> null,
+            'message'=>'No existe registro. '
         ],404);
+    }
+
+
+    public function ingresarVendedor(Request $r){
+        $ip = $r->ip();
+        $executed = RateLimiter::attempt($ip,$perTwoMinutes = 3,function() {});
+        if (!$executed)
+            return response()->json(['success'=>false, 'message'=>'Demasiadas peticiones. Espere 1 minuto.' ],500);
+
+        $validator = Validator::make($r->all(), [
+            'cedula' => 'required|unique:vendedores,cedula',
+            'punto' => 'required|max:4',
+        ],['cedula.unique'=>'Vendedor ya ingresado.']);
+
+        if ($validator->fails())
+            return response()->json([
+                'success'=>false,
+                'message'=> $validator->errors()->first()
+            ], 400);
+
+
+        $farmaService = new FarmaService();
+        $res = (object) $farmaService->cliente($r->cedula);
+        $data = (object)$res->data;
+
+        if(property_exists($data,'result') && count($data->result) > 0){
+            $ficha = $data->result[0];
+            if($ficha['esFuncionario'] === 'S'){
+                $datoInsertar = [
+                'cedula' => $r->cedula,
+                'nombre' =>$ficha['persNombre'],
+                'punto' => $r->punto
+                ];
+
+                Vendedor::create($datoInsertar);
+
+                return response()->json([
+                    'success'=>true,
+                    'message'=> 'Ingresado. Ya dispone de un QR vendedor.'
+                ]);
+            }
+        }
+
+        return response()->json([
+            'success'=>true,
+            'message'=>'Error al ingresar. Contacte con nosotros.',
+            'results'=>null
+        ],500);
+
+
     }
 }
--- routes/farma.php
+++ routes/farma.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Farma\VendedoresController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/generar-qr',[VendedoresController::class,'generarQRVendedor'])->name('farma_generar_qr');
+Route::post('/ingresar-vendedor',[VendedoresController::class,'ingresarVendedor'])->name('farma_ingresar_vendedor');
