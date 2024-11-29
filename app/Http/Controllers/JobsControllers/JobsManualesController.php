8d87e4e7e8c2fa4d59f309eb32b191224760b5f1
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-11-29 12:55:58 -0300

    jobs

--- app/Http/Controllers/JobsControllers/JobsManualesController.php
+++ app/Http/Controllers/JobsControllers/JobsManualesController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class JobsManualesController extends Controller
 {
     public function actualizarTarjetas(){
-        ActualizarTarjetasJobs::dispatch()->onConnection('database');
-        return response()->json(['success'=>true,'message'=>'Actualizacion en segundo plano']);
+        // ActualizarTarjetasJobs::dispatch()->onConnection('database');
+        return response()->json(['success'=>false,'message'=>'Job ya ha sido procesado.'],400);
     }
 }
