9f3c5a39980eabcad6e10f5743633f940f6da582
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-01-21 18:47:35 -0300

    de hoy

--- routes/console.php
+++ routes/console.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::job(new CumpleaniosJobs());
 Schedule::job(new UpdateSolicitudesJobs());
 Schedule::job(new ProcesarVentasDelDiaFarmaJobs(Carbon::yesterday()->format('Y-m-d')));
+Schedule::job(new ProcesarVentasDelDiaFarmaJobs(Carbon::now()->format('Y-m-d')));
 
 
 
