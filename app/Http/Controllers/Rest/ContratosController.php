7b9647cd3fa56434105edf64369eb0da49d0bc42
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-02-07 15:03:05 -0300

    cliente

--- app/Http/Controllers/Rest/ContratosController.php
+++ app/Http/Controllers/Rest/ContratosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Rest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cliente;
 use App\Services\FarmaService;
 use App\Traits\ContratosBlupyFarmaTraits;
 use Illuminate\Http\Request;
@@ -21,6 +22,10 @@ class ContratosController extends Controller
         $res = $this->consultarContratoBlupyPorDocumentoEnFarma($documento);
         if(!$res->success)
             return response()->json(['success'=>false,'message'=>'No se encontraron contratos para el documento ingresado'], 404);
+        $cliente = Cliente::where('cedula',$documento)->select('foto_ci_frente','cedula','id')->first();
+        if($cliente){
+            $res->results['cliente'] = $cliente;
+        }
 
         return response()->json(['success'=>true,'results'=>$res->results],$res->status);
     }
