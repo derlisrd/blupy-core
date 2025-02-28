363b8caba3c1b986639e3910160a8d86e22cdd79
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-02-28 11:57:19 -0300

    probar subir foto

--- app/Http/Controllers/Rest/ContratosController.php
+++ app/Http/Controllers/Rest/ContratosController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Rest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Cliente;
-use App\Services\FarmaService;
 use App\Traits\ContratosBlupyFarmaTraits;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
