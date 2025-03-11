6c7c9685626b3e32b7763c8d8d839ced3ba324d1
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-03-10 22:08:31 -0300

    contrato

--- app/Http/Controllers/Rest/ContratosController.php
+++ app/Http/Controllers/Rest/ContratosController.php
@@ -23,10 +23,12 @@ class ContratosController extends Controller
             return response()->json(['success'=>false,'message'=>'No se encontraron contratos para el documento ingresado'], 404);
 
         $cliente = Cliente::where('clientes.cedula',$documento)
-        ->where('s.tipo','=',1)
+        ->where('s.tipo',1)
+        ->whereIn('s.estado_id',[3,5,7])
         ->join('solicitud_creditos as s','clientes.id','=','s.cliente_id')
         ->select('clientes.foto_ci_frente','clientes.cedula','clientes.id','clientes.selfie','clientes.nombre_primero',
-        'clientes.apellido_primero','clientes.celular','s.tipo','s.estado','s.codigo','s.estado_id','s.created_at')->first();
+        'clientes.apellido_primero','clientes.celular','s.tipo','s.estado','s.codigo','s.estado_id','s.created_at')
+        ->first();
         $results = [
             'contratos'=>$res->results,
             'cliente'=>$cliente
@@ -45,7 +47,8 @@ class ContratosController extends Controller
             return response()->json(['success'=>false,'message'=>'No se encontraron contratos para el documento ingresado'], 404);
 
         $cliente = Cliente::where('s.codigo',$codigo)
-        ->where('s.tipo','=',1)
+        ->where('s.tipo',1)
+        ->whereIn('s.estado_id',[3,5,7])
         ->join('solicitud_creditos as s','clientes.id','=','s.cliente_id')
         ->select('clientes.foto_ci_frente','clientes.cedula','clientes.id','clientes.selfie','clientes.nombre_primero',
         'clientes.apellido_primero','clientes.celular','s.tipo','s.estado','s.codigo','s.estado_id','s.created_at')->first();
