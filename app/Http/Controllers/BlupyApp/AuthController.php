7a4ee6248c47c06f83e67451cb214f16f463f1c8
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-03-12 09:40:50 -0300

    258

--- app/Http/Controllers/BlupyApp/AuthController.php
+++ app/Http/Controllers/BlupyApp/AuthController.php
@@ -172,7 +172,7 @@ class AuthController extends Controller
 
             $version = $req->version ?? null;
 
-            if($version !== '2.6.4'){
+            if($version == '2.5.8') {
                 return response()->json(['success' => false, 'message' => 'Por favor actualice su app para mayor seguridad'], 400);
             }
 
