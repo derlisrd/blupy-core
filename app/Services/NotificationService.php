d45c16c53586646b517f94563d9bf633998e127d
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-03-17 16:23:22 -0300

    push a todos

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -17,7 +17,7 @@ class NotificationService
             $response = Http::withHeaders([
                 'Content-Type' => 'application/json',
                 'x-api-key' => env('PUSH_SERVICE_API_KEY')
-            ])->post(env('PUSH_SERVICE_URL') . '/send-notification',[
+            ])->post(env('PUSH_SERVICE_URL') . '/send-push-difusion',[
                 'tokens' => [$data['tokens']],
                 'title' => $data['title'],
                 'body' => $data['body'],
