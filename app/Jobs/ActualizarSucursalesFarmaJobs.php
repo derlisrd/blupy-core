79fa7c86d79548ce9d0ddffa618541435ed0d5a6
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-03-24 10:16:37 -0300

    queue

--- app/Jobs/ActualizarSucursalesFarmaJobs.php
+++ app/Jobs/ActualizarSucursalesFarmaJobs.php
@@ -3,13 +3,17 @@
 namespace App\Jobs;
 
 use App\Models\Sucursal;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
 use App\Services\FarmaService;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 class ActualizarSucursalesFarmaJobs implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * Create a new job instance.
