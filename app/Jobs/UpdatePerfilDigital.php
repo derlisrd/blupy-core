26d3c67c39b964e11382ea33933fdb57db2a0a1a
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-03-31 09:48:38 -0300

    job de perfil digital

--- app/Jobs/UpdatePerfilDigital.php
+++ app/Jobs/UpdatePerfilDigital.php
@@ -5,12 +5,15 @@ namespace App\Jobs;
 use App\Models\Cliente;
 use App\Services\InfinitaService;
 use App\Services\SupabaseService;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 
 class UpdatePerfilDigital implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * Create a new job instance.
