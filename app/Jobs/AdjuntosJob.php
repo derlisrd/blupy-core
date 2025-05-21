2bc9dfac997e9de4337c127c20ded7bd4a3d86d1
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-05-21 11:40:24 -0300

    fix: job

--- app/Jobs/AdjuntosJob.php
+++ app/Jobs/AdjuntosJob.php
@@ -7,9 +7,14 @@ use App\Models\Cliente;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
 class AdjuntosJob implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * Create a new job instance.
