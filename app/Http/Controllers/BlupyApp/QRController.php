ddd2bd7ac3f52180e0aa542c7b762a23ad787fd8
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-05-29 10:22:54 -0300

    qr

--- app/Http/Controllers/BlupyApp/QRController.php
+++ app/Http/Controllers/BlupyApp/QRController.php
@@ -37,6 +37,7 @@ class QRController extends Controller
                 'adicional' => $req->adicional,
                 'extranjero' => $cliente->extranjero,
             ];
+            Log::info('numeroCuenta: '.$req->numeroCuenta .' tipo ' . gettype($req->numeroCuenta));
             if($req->numeroCuenta !== '0' || $req->numeroCuenta !== null){
                 $resInfinita = app(InfinitaService::class)->ListarTarjetasPorDoc($cliente->cedula);
                 $infinita = (object)$resInfinita['data'];
