dc521f25e7d8c7cf89ba6b97cccc3795778bd37a
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-06-19 11:17:08 -0300

    zip

--- app/Services/PushExpoService.php
+++ app/Services/PushExpoService.php
@@ -1,25 +1,30 @@
 <?php
 
 namespace App\Services;
+
 use Illuminate\Support\Facades\Http;
 
 class PushExpoService
 {
     private $urlService;
-    public function __construct(){
+    public function __construct()
+    {
         $this->urlService = env('PUSH_SERVICE');
     }
 
-    public function send(array $to, string $title,string $body, array $data){
+    public function send(array $to, string $title, string $body, array $data)
+    {
         try {
             $response = Http::withHeaders([
-                    'Content-Type' => 'application/json',
-                ])->post('https://exp.host/--/api/v2/push/send', [
-                    'to' => $to,
-                    'title' => $title,
-                    'body' => $body,
-                    'data' => $data
-                ]);
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'Accept-Encoding' => 'gzip, deflate',
+            ])->post('https://exp.host/--/api/v2/push/send', [
+                'to' => $to,
+                'title' => $title,
+                'body' => $body,
+                'data' => $data
+            ]);
             return  $response->json();
         } catch (\Throwable $th) {
             return false;
