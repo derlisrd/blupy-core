326e3d5fa68c57758e23bd680d1420dc5284969b
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-08-07 16:04:51 -0300

    fix: route wa

--- routes/api.php
+++ routes/api.php
@@ -49,7 +49,7 @@ Route::prefix('/scan')->group(function(){
 
 Route::prefix('/validar')->group(function(){
     Route::get('/enviarme-codigo-sms',[ValidacionesController::class,'enviarmeCodigoPorSmsParaValidarNroTelefono']);
-    Route::get('/reenviarme-codigo-wa',[ValidacionesController::class,'reEnviarmeCodigoPorWaParaValidarNroTelefono']);
+    Route::get('/reenviar-codigo-wa',[ValidacionesController::class,'reEnviarmeCodigoPorWaParaValidarNroTelefono']);
     Route::post('/confimar-codigo-sms',[ValidacionesController::class,'confirmarCodigoParaValidarNroTelefono']);
 });
 /**
