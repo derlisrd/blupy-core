b1279590e6d3abfad459471d0761bb1ec3003403
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-08-11 23:16:41 -0300

    mejora

--- app/Http/Controllers/BlupyApp/CuentasController.php
+++ app/Http/Controllers/BlupyApp/CuentasController.php
@@ -25,76 +25,117 @@ class CuentasController extends Controller
 
 
 
-    public function tarjetas(Request $req){
+    public function tarjetas(string $cedula, int $extranjero, string $codigo_farma, int $franquicia)
+    {
+
         $results = [];
-        $user = $req->user();
-        $cedula = $user->cliente->cedula;
-        $resInfinita = (object) $this->infinitaService->ListarTarjetasPorDoc($cedula);
-        $infinita = (object)$resInfinita->data;
-        if(property_exists( $infinita,'Tarjetas')){
-            foreach ($infinita->Tarjetas as $val) {
-                array_push($results, [
-                    'id'=>2,
-                    'descripcion'=>'Blupy crédito digital',
-                    'otorgadoPor'=>'Mi crédito S.A.',
-                    'tipo'=>1,
-                    'bloqueo'=> $val['MTBloq'] === "" ? false : true,
-                    'condicion'=>'Contado',
-                    'condicionVenta'=>1,
-                    'cuenta' => $val['MaeCtaId'],
-                    'numeroTarjeta'=>$val['MTNume'],
-                    'linea' => (int)$val['MTLinea'],
-                    'pagoMinimo'=> (int) $val['MCPagMin'],
-                    'deuda' => (int) $val['MTSaldo'],
-                    'disponible' => (int) $val['MTLinea'] - (int) $val['MTSaldo'],
-                    'alianzas' => []
-                ]);
+
+        // Manejo de errores para API Infinita
+        try {
+            $resInfinita = $this->infinitaService->ListarTarjetasPorDoc($cedula);
+            $infinita = (object)$resInfinita['data'];
+
+            if (isset($infinita->Tarjetas)) {
+                foreach ($infinita->Tarjetas as $val) {
+                    array_push($results, [
+                        'id' => 2,
+                        'descripcion' => 'Blupy Digital',
+                        'otorgadoPor' => 'Mi crédito S.A.',
+                        'tipo' => 1,
+                        'emision' => $val['MTFEmi'],
+                        'bloqueo' => $val['MTBloq'] === "" ? false : true,
+                        'condicion' => 'Contado',
+                        'condicionVenta' => 1,
+                        'cuenta' => $val['MaeCtaId'],
+                        'principal' => $val['MTTipo'] === 'P',
+                        'adicional' => $val['MTTipo'] === 'A',
+                        'numeroTarjeta' => $val['MTNume'],
+                        'linea' => (int)$val['MTLinea'],
+                        'pagoMinimo' => (int) $val['MCPagMin'],
+                        'deuda' => (int) $val['MTSaldo'],
+                        'disponible' => (int) $val['MTLinea'] - (int) $val['MTSaldo'],
+                        'alianzas' => [],
+                    ]);
+                }
             }
+        } catch (\Exception $e) {
+            // Log del error pero continúa la ejecución
+            Log::error('Error al consultar API Infinita: ' . $e->getMessage(), [
+                'cedula' => $cedula,
+                'trace' => $e->getTraceAsString()
+            ]);
+
+            // Opcionalmente enviar notificación a Supabase
+            SupabaseService::LOG('infinita_api_error', $e->getMessage());
+        }
+        // Manejo de errores para API Farma
+        $resFarma = null;
+        try {
+            if ($extranjero == 1) {
+                $resFarma = $this->farmaService->clientePorCodigo($codigo_farma);
+            } else {
+                $resFarma = $this->farmaService->cliente($cedula);
+            }
+        } catch (\Exception $e) {
+            // Log del error pero continúa la ejecución
+            Log::error('Error al consultar API Farma: ' . $e->getMessage(), [
+                'cedula' => $cedula,
+                'extranjero' => $extranjero,
+                'codigo_farma' => $codigo_farma,
+                'trace' => $e->getTraceAsString()
+            ]);
+
+            SupabaseService::LOG('farma_api_error', $e->getMessage());
+
+            // Continúa con $resFarma = null
         }
 
-        $resFarma = $this->farmaService->cliente($cedula);
-        $farma = (object) $resFarma['data'];
+        // El resto del código se mantiene igual
+        if (!$resFarma) {
+            return $results;
+        }
 
-        if(property_exists( $farma,'result')){
+        $farma = (object) $resFarma['data'];
+        if (property_exists($farma, 'result')) {
             foreach ($farma->result as $val) {
                 $alianzas = [];
-                foreach($val['alianzas'] as $alianza){
-                    //if($alianza['frpaCodigo'] === 129 ){
-                        array_push($alianzas,[
-                            'codigo'=>$alianza['codigoAdicional'],
-                            'nombre'=> $alianza['alianza'],
-                            'descripcion'=> $alianza['alianza'],
-                            'formaPagoCodigo'=> $alianza['frpaCodigo'],
-                            'formaPago'=>$alianza['formaPago']
+                foreach ($val['alianzas'] as $alianza) {
+                    if ($alianza['frpaCodigo'] > 126) {
+                        array_push($alianzas, [
+                            'codigo' => $alianza['codigoAdicional'],
+                            'nombre' => $alianza['alianza'],
+                            'descripcion' => $alianza['alianza'],
+                            'formaPagoCodigo' => $alianza['frpaCodigo'],
+                            'formaPago' => $alianza['formaPago']
                         ]);
-                    //}
+                    }
                 }
-                //if(count($alianzas)>0 || $val['esFuncionario'] ==='S'){
+                if (count($alianzas) > 0 || $val['esFuncionario'] === 'S' || $franquicia === 1) {
                     array_push($results, [
-                        'id'=>1,
-                        'descripcion'=>'Blupy crédito 1 día',
-                        'otorgadoPor'=>'Farma S.A.',
-                        'tipo'=>0,
-                        'condicion'=>'credito',
-                        'condicionVenta'=>2,
+                        'id' => 1,
+                        'descripcion' => $val['esFuncionario'] === 'S' ? 'Blupy Farma' : 'Blupy Alianza',
+                        'otorgadoPor' => $val['esFuncionario'] === 'S' ? 'Farma S.A.' : 'Farma por alianza',
+                        'tipo' => 0,
+                        'emision' => null,
+                        'condicion' => 'credito',
+                        'condicionVenta' => 2,
                         'cuenta' => null,
-                        'bloqueo'=> false,
-                        'numeroTarjeta'=>null,
-                        'linea' => $val['limiteCreditoTotal'],
-                        'pagoMinimo'=> null,
+                        'bloqueo' => false,
+                        'numeroTarjeta' => null,
+                        'linea' => $val['limiteCreditoTotal'], //$val['clerLimiteCredito'] +  $val['clerLimiteCreditoAdic']
+                        'pagoMinimo' => null,
                         'deuda' => $val['pendiente'],
-                        'disponible' => $val['saldoDisponible'],
+                        'disponible' => $val['saldoDisponible'], // ($val['clerLimiteCredito'] +  $val['clerLimiteCreditoAdic']) - $val['pendiente'],
                         'alianzas' => $alianzas
                     ]);
-                //}
+                }
             }
         }
 
-
-
         return response()->json([
-            'success'=>true,
-            'results'=>$results
+            'success' => true,
+            'results' => $results,
+            'message' => ''
         ]);
     }
 
