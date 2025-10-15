950bc4c6273a03d352c2fedf7edf3e402149c623
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-10-15 11:46:27 -0300

    fix: padding

--- resources/views/terminos.blade.php
+++ resources/views/terminos.blade.php
@@ -17,7 +17,7 @@
             max-width: 800px;
             margin: auto;
             background: #fff;
-            padding: 30px;
+            padding: 18px;
             box-shadow: 0 2px 8px rgba(0,0,0,0.05);
             border-radius: 8px;
         }
