54b3ddd5e62207aec91bb4fd0de376656a2a4394
derlisrd <derlisruizdiaz@hotmail.com>
2025-10-30 11:49:55 -0300

    ventas innecesarias

--- routes/console.php
+++ routes/console.php
@@ -1,17 +1,15 @@
 <?php
 
 use App\Jobs\CumpleaniosJobs;
-use App\Jobs\ProcesarVentasDelDiaFarmaJobs;
-use Carbon\Carbon;
+/* use App\Jobs\ProcesarVentasDelDiaFarmaJobs;
+use Carbon\Carbon; */
 use Illuminate\Support\Facades\Schedule;
 
 
 Schedule::job(new CumpleaniosJobs());
 //Schedule::job(new UpdateSolicitudesJobs());
-Schedule::job(new ProcesarVentasDelDiaFarmaJobs(Carbon::yesterday()->format('Y-m-d')));
-Schedule::job(new ProcesarVentasDelDiaFarmaJobs(Carbon::now()->format('Y-m-d')));
-
-
+//Schedule::job(new ProcesarVentasDelDiaFarmaJobs(Carbon::yesterday()->format('Y-m-d')));
+//Schedule::job(new ProcesarVentasDelDiaFarmaJobs(Carbon::now()->format('Y-m-d')));
 
 //->dailyAt('00:40');
 //->timezone('America/Asuncion')
