038b356bc12ce07cf1560e0c9334e884d5db828d
derlisrd <derlisruizdiaz@hotmail.com>
2026-01-20 08:41:25 -0300

    dispositivo

--- app/Http/Controllers/BlupyApp/Auth/LoginController.php
+++ app/Http/Controllers/BlupyApp/Auth/LoginController.php
@@ -69,7 +69,7 @@ class LoginController extends Controller
             
 
             // 6. Verificar dispositivo de confianza (solo para rol 0)
-             if ($cliente->user->rol === 0) {
+             /* if ($cliente->user->rol === 0) {
                 $user = $cliente->user;
                 $dispositoDeConfianza = Device::where('user_id', $user->id)
                         ->where('desktop', $req->desktop)
@@ -94,7 +94,7 @@ class LoginController extends Controller
                             'ip' => $req->ip()
                         ]);
                         $dispositoDeConfianza->touch();
-            }   
+            }    */
             $cliente->user->update([
                 'intentos' => 0,
                 'ultimo_ingreso' => now(),
