4b7ed4a5dd8079cfbcd97b6dd08435f311337fa1
derlisrd <derlisruizdiaz@hotmail.com>
2026-03-24 09:00:53 -0300

    fix: alianzas

--- app/Http/Controllers/BlupyApp/CuentasController.php
+++ app/Http/Controllers/BlupyApp/CuentasController.php
@@ -112,9 +112,9 @@ class CuentasController extends Controller
                 $funcionario = $tarjetasFarma['funcionario'];
                 $alianza = $tarjetasFarma['alianza'] ?? null;
 
-                if($funcionario === false && $alianza === null){
-                    return [];
-                }
+                //if($funcionario === false && $alianza === null){
+                  //  return [];
+                //}
 
                 $linea = $tarjetasFarma['clerLimiteCredito'];
 
