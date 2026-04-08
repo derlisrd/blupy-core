4413c93bd279a87a4d7d0ddd39eeab3a234b690f
derlisrd <derlisruizdiaz@hotmail.com>
2026-04-08 08:32:16 -0300

    nomas mas push

--- routes/console.php
+++ routes/console.php
@@ -6,7 +6,7 @@ use Carbon\Carbon; */
 use Illuminate\Support\Facades\Schedule;
 
 
-Schedule::job(new CumpleaniosJobs());
+//Schedule::job(new CumpleaniosJobs());
 //Schedule::job(new UpdateSolicitudesJobs());
 //Schedule::job(new ProcesarVentasDelDiaFarmaJobs(Carbon::yesterday()->format('Y-m-d')));
 //Schedule::job(new ProcesarVentasDelDiaFarmaJobs(Carbon::now()->format('Y-m-d')));
