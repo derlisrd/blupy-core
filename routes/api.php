91ea2c974b5d388445e6b95bec0dfdf487e70788
derlisrd <derlisruizdiaz@hotmail.com>
2026-05-13 15:42:19 -0300

    solicitudes

--- routes/api.php
+++ routes/api.php
@@ -144,11 +144,13 @@ Route::middleware('auth:api')->group(function(){
     Route::get('/movimientos',[MovimientosController::class,'movimientos'])->name('api_movimientos');
     Route::get('/extracto',[CuentasController::class,'extracto'])->name('api_extracto');
 
-    Route::get('/solicitudes',[SolicitudesController::class,'solicitudes'])->name('api_solicitudes');
-    Route::post('/solicitar-credito',[SolicitudesController::class,'solicitarCredito'])->name('api_solicitar_credito');
-    Route::post('/solicitar-ampliacion',[SolicitudesController::class,'solicitarAmpliacion'])->name('api_solicitar_ampliacion');
-    Route::post('/agregar-adicional',[SolicitudesController::class,'agregarAdicional'])->name('api_agregar_adicional');
-    Route::get('/verificar-estado-solicitud',[SolicitudesController::class,'verificarEstadoSolicitud'])->name('api_verificar_estado_solicitud');
+    Route::prefix('/solicitudes')->group(function () {
+        Route::get('/', [SolicitudesController::class, 'solicitudes'])->name('api_solicitudes');
+        Route::post('/solicitar-credito', [SolicitudesController::class, 'solicitarCredito'])->name('api_solicitar_credito');
+        Route::post('/solicitar-ampliacion', [SolicitudesController::class, 'solicitarAmpliacion'])->name('api_solicitar_ampliacion');
+        Route::post('/agregar-adicional', [SolicitudesController::class, 'agregarAdicional'])->name('api_agregar_adicional');
+        Route::get('/verificar-estado-solicitud', [SolicitudesController::class, 'verificarEstadoSolicitud'])->name('api_verificar_estado_solicitud');
+    });
 
     Route::put('/cambiar-contrasena',[UserPrivate::class,'cambiarContrasena'])->name('api_cambiar_contrasena');
 
