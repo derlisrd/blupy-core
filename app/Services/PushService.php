8674dbd38c8c6cacaaadab8a98ea34525cf7ecc9
derlisrd <derlisruizdiaz@hotmail.com>
2026-06-04 18:34:34 -0300

    instanciando cosas

--- app/Services/PushService.php
+++ app/Services/PushService.php
@@ -5,17 +5,20 @@ namespace App\Services;
 use Kreait\Firebase\Contract\Messaging;
 use Kreait\Firebase\Exception\MessagingException;
 use Kreait\Firebase\Messaging\CloudMessage;
-
+use Kreait\Firebase\Factory;
 class PushService
 {
     /**
      * Create a new class instance.
      */
+    
     public function __construct()
     {
+        
     }
 
-    public function sendPushMulti(array $deviceTokens, array $data = [], string $title, string $body){
+
+    public function sendPushMulti(array $deviceTokens, string $title, string $body, array $data = []){
         $messaging = app(Messaging::class);
         $message = CloudMessage::new()->withNotification([
             'title' => $title,
@@ -31,11 +34,14 @@ class PushService
         }
     }
 
-    public function sendPushNotification($token, $title, $body, $data = [])
+    public function sendPushNotification(string $token, string $title, string $body, $data = [])
     {
-        $messaging = app(Messaging::class); // ← Obtener la instancia de Messaging
+
+        $factory = (new Factory)->withServiceAccount(storage_path('/app/firebase.json'));
+        $messaging = $factory->createMessaging();
+        //$messaging = app(Messaging::class); // ← Obtener la instancia de Messaging
         $message = CloudMessage::fromArray([
-            'token' => $token,
+            'token' => 'dA1EGxTwSbaw0vjnGPKzji:APA91bE2nhpDlAcSQKNaVp7gvcMrQJC8DWza-xLXwJbHQunAq8AYLuBTnUDIUw6FWTNdKxwyKMVtyCbitGiQ1v5DKpMpkERsvH2oHjI9AwPy6OKRkknQSeEcQZ0Ge8_WMuitkun4PX0s',
             'notification' => [
                 'title' => $title,
                 'body'  => $body,
