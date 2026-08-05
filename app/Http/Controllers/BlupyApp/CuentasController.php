5e213ba35ac308c66ce31aeb81a367cb4197200f
derlisrd <derlisruizdiaz@hotmail.com>
2026-08-05 10:22:45 -0300

    a

--- app/Http/Controllers/BlupyApp/CuentasController.php
+++ app/Http/Controllers/BlupyApp/CuentasController.php
@@ -236,10 +236,10 @@ class CuentasController extends Controller
                 $tarjetasFarma = $farmaCardDataF['result'];
                 if ($tarjetasFarma != null) {
 
-                    $isFuncionario = !empty($tarjetasFarma['funcionario']) || $franquicia === 1;
+                    $funcionario =  $tarjetasFarma['funcionario'];  //!empty($tarjetasFarma['funcionario']) || $franquicia === 1;
                     $alianza = $tarjetasFarma['alianza'] ?? null;
 
-                    if (!$isFuncionario && $alianza === null) {
+                    if (!$funcionario && $alianza === null) {
                         return [];
                     }
 
